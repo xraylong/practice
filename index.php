1551adf7ec8f295f578d35f0d012728977770029
xraylong <38187507+xraylong@users.noreply.github.com>
2018-12-31 17:19:33 -0500

    Fixed indenting

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ a.change_link:active {
 <!-- Body includes the html code-->			
 <body style="background-color:powderblue;">
 	<!-- headers can have 6 headers h1, h2, h3, h4, etc...-->
-	<h1 id = "top">Anthony Long</h1>
+	<h1 id="top">Anthony Long</h1>
 
 	<!-- Paragraph is the basics of text -->
 	<p class="padding_1st">I was about to post this quote by Socrates: "An unexamined life is not worth living". Then I thought of contrary, an examined life. This can lead to one not believing themselves not to be worth living either. Or simply put, suicidal people. They examine their lives to the point of death.
@@ -64,26 +64,28 @@ I've never heard of any animal other than a human commit suicide (edit: seems th
 
 Is our ability to examine our own lives special/unique? or are we just too hard on ourselves than other animals? Should we just go back to the caves away from the stresses of life? Too many questions to ask.</p>
 
-<strong>Bold</strong>
-<em>Italic</em>
-<mark>Highlight</mark>
-<small>Small text</small>
-<ins>Underline</ins>
-<del>Strike</del>
-<sub>Subscript</sub>
-<sup>SuperScript</sup>
-<q>This is a quote</q>
-<blockquote cite="http://www.worldwildlife.org/who/index.html">
-Blockquotes indent and also give reference to source
-</blockquote>
-<address style="color: rgba(230, 40, 40, 50);">
-Written by John Doe.<br> 
-Visit us at:<br>
-Example.com<br>
-Box 564, Disneyland<br>
-USA
-</address>
-<bdo dir="rtl">This line will be written from right to left</bdo> <!-- rtl means right to left -->
+<section>
+	<strong>Bold</strong>
+	<em>Italic</em>
+	<mark>Highlight</mark>
+	<small>Small text</small>
+	<ins>Underline</ins>
+	<del>Strike</del>
+	<sub>Subscript</sub>
+	<sup>SuperScript</sup>
+	<q>This is a quote</q>
+	<blockquote cite="http://www.worldwildlife.org/who/index.html">
+	Blockquotes indent and also give reference to source
+	</blockquote>
+	<address style="color: rgba(230, 40, 40, 50);">
+	Written by John Doe.<br> 
+	Visit us at:<br>
+	Example.com<br>
+	Box 564, Disneyland<br>
+	USA
+	</address>
+	<bdo dir="rtl">This line will be written from right to left</bdo> <!-- rtl means right to left -->
+</section>
 
 <br>
 <hr>
